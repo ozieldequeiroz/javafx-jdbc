bce92b666732d91e3ab4a369d803e0fd32f0960c
ozieldequeiroz <o.aires10@gmail.com>
2019-07-12 22:03:05 -0300

    Departament view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -33,7 +33,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartamentAction() {
-		System.out.println("onMenuItemDepartamentAction");
+		loadView("/gui/DepartamentList.fxml");
 	}
 	
 	@FXML
