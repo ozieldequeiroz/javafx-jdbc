f3eb24024e61342739ad5d17af94836355317151
ozieldequeiroz <o.aires10@gmail.com>
2019-07-22 11:23:22 -0300

    Saving a New Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -5,16 +5,26 @@ import java.util.ResourceBundle;
 
 import javax.swing.SpringLayout.Constraints;
 
+import db.DbException;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	
+	@FXML
 	private Department entity;
+	
+	@FXML
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -36,13 +46,40 @@ public class DepartmentFormController implements Initializable {
 	}
 	
 	@FXML
-	public void onBtSaveAction() {
-		System.out.println("onBtSaveAction");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFromData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+		
+	
+	private Department getFromData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -9,5 +9,13 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event){
 		return (Stage)((Node) event.getTarget()).getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 }
